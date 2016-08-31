db3d72c2db009437e8a8356ecda334308f3d3165
SeiYukinari <sei.yukinari@nextremer.com>
2016-08-31 16:00:20 +0900

    Bootstrap適用

--- src/js/components/presentors/CountArea/Counter.js
+++ src/js/components/presentors/CountArea/Counter.js
@@ -1,3 +1,6 @@
+import { ButtonToolbar, ButtonGroup, Button } from "react-bootstrap";
+
+
 export default class Counter extends React.Component {
   constructor( props ) {
     super( props );
@@ -5,20 +8,35 @@ export default class Counter extends React.Component {
 
   render() {
     return (
+
         <div>
           <h1>Counter example</h1>
           <p>
-            Clicked: { this.props.count.counter } times
-            {" "}
-            <button className="increment" onClick={ this.props.count.onIncrement }>+</button>
-            {" "}
-            <button className="decrement" onClick={ this.props.count.onDecrement }>-</button>
-            {" "}
-            <button className="incrementIfOdd" onClick={ this.props.count.onIncrementIfOdd }>IncrementIfOdd if odd
-            </button>
-            {" "}
-            <button className="incrementAsync" onClick={ this.props.count.onIncrementAsync }>IncrementAsync async
-            </button>
+            <h3>Clicked: { this.props.count.counter } times</h3>
+            <ButtonToolbar>
+              <ButtonGroup bsSize="large">
+                <Button bsStyle="success"
+                        className="increment"
+                        onClick={ this.props.count.onIncrement }>
+                  +
+                </Button>
+                <Button bsStyle="primary"
+                        className="decrement"
+                        onClick={ this.props.count.onDecrement }>
+                  -
+                </Button>
+                <Button bsStyle="warning"
+                        className="incrementIfOdd"
+                        onClick={ this.props.count.onIncrementIfOdd }>
+                  IncrementIfOdd if odd
+                </Button>
+                <Button bsStyle="danger"
+                        className="incrementAsync"
+                        onClick={ this.props.count.onIncrementAsync }>
+                  IncrementAsync async
+                </Button>
+              </ButtonGroup>
+            </ButtonToolbar>
           </p>
         </div>
     );
